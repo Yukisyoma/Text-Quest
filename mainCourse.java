335148246d28bba3eafdd90ba2f2494395b4c43e
Yukisyoma <krolowich@gmail.com>
2018-04-19 17:51:00 +0300

    Add files via upload

---
+++ mainCourse.java
@@ -0,0 +1,23 @@
+package simpleGames;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+public class mainCourse {
+  
+  public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    FileReader choices = new FileReader(choices), descriptions = new FileReader(descriptions);
+    
+    boolean running = true;
+    
+    while(running) {
+      
+      
+      
+    }
+  }
+  
+}
